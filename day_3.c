c918fe4f6c81a06eb315c805be2b49307052e1c9
Jerry Arciaga <jerryarciaga11gmail.com>
2020-12-16 17:03:37 +0000

    TODO: Fix Special Case

--- day_3.c
+++ day_3.c
@@ -2,38 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 
+int tree_count(char *inputFile, int right, int lines);
+
 int main() {
-    FILE *input;
+    char *inputFile = "input.txt";
     int lines = 323; // wc -l input.txt
-    
+    int trees = 0;
+
     /* PART ONE */
-    // Open the file
-    input = fopen("input.txt", "r");
-    if(input == NULL) {
-        fprintf(stderr, "Error: File not found\n");
-        return 1;
+    trees = tree_count(inputFile, 3, lines);
+    printf("Number of trees encountered (Part 1): %d\n", trees);
+    /* END PART ONE */
+    
+    /* PART TWO */
+    unsigned long final_answer = 1;
+    final_answer *= (unsigned long)tree_count(inputFile, 1, lines);
+    final_answer *= (unsigned long)tree_count(inputFile, 3, lines);
+    final_answer *= (unsigned long)tree_count(inputFile, 5, lines);
+    final_answer *= (unsigned long)tree_count(inputFile, 7, lines);
+    
+    // Special Case: Right 1, down 2
+    FILE *input = fopen(inputFile, "r");
+    int right = 0;
+    char line[255];
+    for(int i = 0; i <= (lines/2) + 2; ++i) {
+        right++;
+        fgets(line, 255, input);
+        line[strlen(line)-1] = '\0';
+        fgets(line, 255, input);
+        line[strlen(line)-1] = '\0';
+
+        trees += (line[right % strlen(line)] == '#');
+    }
+    final_answer *= trees;
+    printf("Number of trees encountered (Part 2): %lu\n", final_answer);
+    fclose(input);
+    /* END PART TWO */
+
+    return 0;
+}
+
+// Function assumes down = 1
+int tree_count(char *inputFile, int right, int lines) {
+    FILE *input = fopen(inputFile, "r");
+    int trees;
+
+    if(inputFile == NULL) {
+        fprintf(stderr, "File not recognized.\n");
+        return -1;
     }
     else {
         char line[255];
-        int right = 0;
-        int trees = 0;
+        int index = 0;
+        trees = 0;
 
-        // We know that the first space is open; no need to be extra
         fgets(line, 255, input);
-        line[strlen(line)-1] ='\0';
-
         for(int i = 0; i < lines; ++i) {
-            // Extract line from file
-            fgets(line,255, input);
+            fgets(line, 255, input);
             line[strlen(line)-1] = '\0';
 
-            // Go right three times, then read next line/down one time
-            right += 3;
-            trees += (line[right % strlen(line)] == '#') ? 1 : 0;
+            index += right;
+            trees += (line[index % strlen(line)] == '#');
         }
-        printf("Number of trees encountered: %d", trees);
+        
         fclose(input);
     }
 
-    return 0;
+    return trees;
 }
